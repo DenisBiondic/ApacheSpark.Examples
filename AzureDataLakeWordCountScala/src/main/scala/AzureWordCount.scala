5b8a3a26eaf24b2b5d7f61318e6d6662fc8b7faf
Denis Biondic <denis.biondic@conplement.de>
2018-11-19 20:33:04 +0100

    feat: simple number count

--- AzureDataLakeWordCountScala/src/main/scala/AzureWordCount.scala
+++ AzureDataLakeWordCountScala/src/main/scala/AzureWordCount.scala
@@ -6,26 +6,12 @@ object AzureWordCount {
   def main(args: Array[String]) = {
     println("Starting the word counter big data application :)")
 
-//    assert(args.length > 0, "Expected two input arguments, input file and output folder!")
-//
-//    val inputFile = args(0)
-//    val outputFolder = args(1)
-//
-//    println(s"Input file $inputFile, output folder $outputFolder")
-//
-//    val conf = new SparkConf().setAppName("Word Counter")
-//    val sc = new SparkContext(conf)
-//
-//    sc.textFile(inputFile)
-//      .flatMap(line => line.split(" "))
-//      .map(word => (word, 1))
-//
-//      // normal reduce would be written as reduce((accumulator, newValue) => accumulator + newValue)
-//      // However, since Spark reduce distributed nature is both associative and commutative,
-//      // there is no guarantee that value1 or value2 is the accumulator
-//      .reduceByKey((value1, value2) => value1 + value2)
-//
-//      .sortBy(kvPair => kvPair._2, false)
-//      .saveAsTextFile(outputFolder)
+    val conf = new SparkConf().setAppName("Azure Word Counter")
+    val sc = new SparkContext(conf)
+
+    val x = 1 to 10 toList
+
+    val result = sc.parallelize(x, 10).map(x => x*2).reduce((a,b) => a+b)
+    println("----------------- Result: " + result)
   }
 }
