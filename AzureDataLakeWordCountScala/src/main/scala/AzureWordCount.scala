d37917446552632706d82c864314e4b8bdaea327
Denis Biondic <denis.biondic@conplement.de>
2018-11-19 20:16:18 +0100

    feat: inital code for azure data lake example

---
+++ AzureDataLakeWordCountScala/src/main/scala/AzureWordCount.scala
@@ -0,0 +1,31 @@
+package main
+
+import org.apache.spark.{SparkConf, SparkContext}
+
+object AzureWordCount {
+  def main(args: Array[String]) = {
+    println("Starting the word counter big data application :)")
+
+//    assert(args.length > 0, "Expected two input arguments, input file and output folder!")
+//
+//    val inputFile = args(0)
+//    val outputFolder = args(1)
+//
+//    println(s"Input file $inputFile, output folder $outputFolder")
+//
+//    val conf = new SparkConf().setAppName("Word Counter")
+//    val sc = new SparkContext(conf)
+//
+//    sc.textFile(inputFile)
+//      .flatMap(line => line.split(" "))
+//      .map(word => (word, 1))
+//
+//      // normal reduce would be written as reduce((accumulator, newValue) => accumulator + newValue)
+//      // However, since Spark reduce distributed nature is both associative and commutative,
+//      // there is no guarantee that value1 or value2 is the accumulator
+//      .reduceByKey((value1, value2) => value1 + value2)
+//
+//      .sortBy(kvPair => kvPair._2, false)
+//      .saveAsTextFile(outputFolder)
+  }
+}
